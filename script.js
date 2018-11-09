3263df982ca56db006ed227094a0f7fa4aad23af
Guilherme-Valle <guidovalle1608@gmail.com>
2018-11-09 14:08:48 -0300

    Adiciona categorias dinamicamente ao select-box

--- script.js
+++ script.js
@@ -5,13 +5,9 @@
 var categories = ['Ação', 'Ficção', 'Drama', 'Comédia', 'Terror', 'Musical', 'Documentário', 'Épico', 'Aventura', 'Suspense',
     'Faroeste', 'Romance', 'Policial', 'Guerra', 'Clássico', 'Animação', 'Infantil'];
 
-var select = document.getElementById("categorie");
-console.log(select);
-for (var i = 0; i < categories.length; i++){
-    var element = document.createElement("element");
-    element.textContent = String(categories[i]);
-    element.value = String(categories[i]);
-    select.appendChild(element);
-}
-
-console.log(categories);
+$.each(categories, function (i, item) {
+    $('.categorie').append($('<option>', {
+        value: item,
+        text : item
+    }));
+});
