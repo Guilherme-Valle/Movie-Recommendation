471347f6b6f508d87763072ad339b5e11fce5fd3
Guilherme-Valle <guidovalle1608@gmail.com>
2018-11-09 14:36:07 -0300

    Atualiza tela

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
  * Array of Categories of Movies
  * @type {string[]}
  */
-var categories = ['Ação', 'Ficção', 'Drama', 'Comédia', 'Terror', 'Musical', 'Documentário', 'Épico', 'Aventura', 'Suspense',
-    'Faroeste', 'Romance', 'Policial', 'Guerra', 'Clássico', 'Animação', 'Infantil'];
+var categories = ['Ação', 'Ficção', 'Drama',
+    'Comédia', 'Terror', 'Musical',
+    'Documentário', 'Épico', 'Aventura', 'Suspense',
+    'Faroeste', 'Romance', 'Policial',
+    'Guerra', 'Clássico', 'Animação', 'Infantil'];
 
+/**
+Put the categories into the select-box.
+*/
 $.each(categories, function (i, item) {
     $('.categorie').append($('<option>', {
         value: item,
