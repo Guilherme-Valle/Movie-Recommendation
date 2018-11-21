e816585e75073dac4e11c12cb05eb9b9434fb79d
Guilherme <guidovalle1608@gmail.com>
2018-11-21 10:31:59 -0300

    Finalização dos filtros

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ $("#recomendation").click(function () {
     var informations = $('#informations').val();
     var year = $('#year').val();
     var categorie = $('#select').val();
+    actors = actors ? actors.split(',') : null;
 
     if (window.XMLHttpRequest){
         xmlhttp = new XMLHttpRequest();
@@ -49,33 +50,61 @@ $("#recomendation").click(function () {
             dom.innerHTML = xmlhttp.responseText;
             /** Lista que será preenchida com os filmes que atenderem a critérios do filtro */
             var moviesOnFilter = [];
-
             /** Itera o objeto DOM e realiza os filtros  */
             for (var i = 1; i < dom.children.length; i++){
-                /** Variável para verificar se o filme atende a algum dos filtros */
-                var flag = false;
+                /** Variável para verificar se o filme atende aos filtros */
+                var flag = 0;
+                /** Quantidade de filtros preenchidos */
+                var filters = 0;
                 /** Busca as informações sobre o filme  */
                 var sinopsisOfMovie = dom.children[i].getElementsByClassName('sinopsis')[0].innerText;
                 var categoriesOfMovie = dom.children[i].getElementsByClassName('categories')[0].children;
                 var yearOfMovie = dom.children[i].children[2].innerText;
-                var actorsOfMovie = dom.children[i].getElementsByClassName('actors')[0].innerText;
+                var actorsOfMovie = dom.children[i].getElementsByClassName('actors')[0].children;
+
+                /** Verifica se o ano do filme corresponde ao do filtro */
                 if (year){
+                    filters++;
                     yearOfMovie = yearOfMovie.split(": ");
                     if (year === yearOfMovie[1]){
-                        flag = true;
+                        flag++;
                     }
                 }
+                /** Verifica se uma das categorias do filme corresponde ao do filtro */
                 if (categorie){
+                    filters++;
                     for (var z = 0; z < categoriesOfMovie.length; z++){
                         if (categoriesOfMovie[z].innerText === categorie){
-                            var flag = true;
+                            flag++;
                         }
                     }
                 }
-
-
-
+                /** Verifica se um dos atores do filtro corresponde a algum dos atores do filme */
+                if (actors){
+                    filters++;
+                    for (var k = 0; k < actors.length; k++){
+                        for (x = 0; x < actorsOfMovie.length; x++){
+                            if (actorsOfMovie[x].innerText === actors[k]){
+                                flag++;
+                            }
+                        }
+                    }
+                }
+                if (informations){
+                    filters++;
+                    /** Transforma a String numa variável RegExp, para usar como uma expressão regular */
+                    var rgxp = new RegExp(informations, "g");
+                    /** Simulação do LIKE no SQL */
+                    if (sinopsisOfMovie.match(rgxp)){
+                        flag++;
+                    }
+                }
+                if (flag === filters){
+                    moviesOnFilter.push(dom.children[i]);
+                }
             }
+
+            console.log(moviesOnFilter);
 
         }
         
