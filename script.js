673102d45fadea8ba9805f934e823ae44b05a759
Guilherme <guidovalle1608@gmail.com>
2018-11-21 01:14:06 -0300

    Inicia manipulação de variáveis

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ Put the categories into the select-box.
 $.each(categories, function (i, item) {
     $('.categorie').append($('<option>', {
         value: item,
-        text : item
+        text: item
     }));
 });
 
@@ -24,20 +24,35 @@ $.each(categories, function (i, item) {
  */
 $("#recomendation").click(function () {
     var xmlhttp;
+
+    /**
+     * Retorna valores dos inputs
+     * @type {jQuery}
+     */
+    var actors = $('#actors').val();
+    var informations = $('#informations').val();
+    var year = $('#year').val();
+    var categorie = $('#select').val();
+
     if (window.XMLHttpRequest){
         xmlhttp = new XMLHttpRequest();
     } else {
         xmlhttp = new ActiveXObject();
     }
 
+    /**
+     * Requisição Ajax para o arquivo HTML com as informações dos filmes.
+     */
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
             var dom = document.createElement('main_content');
             dom.innerHTML = xmlhttp.responseText;
             /** Itera o objeto DOM e realiza os filtros  */
-            for (var i = 0; i < dom.children.length; i++){
+            for (var i = 1; i < dom.children.length; i++){
                 var sinopsis = dom.children[i].getElementsByClassName('sinopsis')[0].innerText;
-                var categorie = dom.children[i].getElementsByClassName('categories')[0].innerText
+                var categorie = dom.children[i].getElementsByClassName('categories')[0].innerText;
+                var year = dom.children[i].children[2].innerText;
+                var actors = dom.children[i].getElementsByClassName('actors')[0].innerText;
             }
 
         }
